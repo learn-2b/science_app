b1e56969a7bad3d23b8e70791bebfc36d923eb37
learn2b <41114251+learn-2b@users.noreply.github.com>
2025-02-22 09:36:12 +0300

    Update app.py

--- app.py
+++ app.py
@@ -51,4 +51,4 @@ def index():
     return render_template("index.html", result=result, graph_json=graph_json)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
